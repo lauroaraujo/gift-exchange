032f11417de79a847d557cb89de073e97e5fd185
Lauro Araujo <lauro.araujo@gmail.com>
2021-11-16 20:44:50 -0500

    Main logic and tests

--- index.test.ts
+++ index.test.ts
@@ -1,10 +1,38 @@
-import {  assertEquals, assertNotStrictEquals } from 'https://deno.land/std@0.114.0/testing/asserts.ts';
-import { randomizeStringArray } from './index.ts'
+import {  assertEquals, assertNotEquals, assertNotStrictEquals } from 'https://deno.land/std@0.114.0/testing/asserts.ts';
+import { randomizeStringArray, selectRandomPairs } from './index.ts'
 
-Deno.test('randomizeStringArray() returns a new array', () => {
-  const original = ['john']
-  const output = randomizeStringArray(original)
+Deno.test('selectRandomPairs returns an empty array if given less than three names', () => {
+  assertEquals(selectRandomPairs([]), [])
+  assertEquals(selectRandomPairs(['lisa']), [])
+  assertEquals(selectRandomPairs(['leon', 'lisa']), [])
+})
 
-  assertNotStrictEquals(original, output)
-  assertEquals(original, output)
+Deno.test('selectRandomPairs returns an array with unique pair objects', () => {
+  const RANDOM_RESULTS = [0.4, 0.3, 0, 0.2, 0.1]
+  const mockRandomFn = () => RANDOM_RESULTS.shift() ?? 0
+
+  const pairs = selectRandomPairs(['Susan', 'Beth', 'Abe', 'Ardi', 'Quan'], mockRandomFn)
+  assertEquals(pairs, [
+    { gives: 'Abe', receives: 'Quan' },
+    { gives: 'Quan', receives: 'Ardi' },
+    { gives: 'Ardi', receives: 'Beth' },
+    { gives: 'Beth', receives: 'Susan' },
+    { gives: 'Susan', receives: 'Abe' }
+  ])
+})
+
+Deno.test('randomizeStringArray() returns a randomized array', () => {
+  const input = ['john', 'jane', 'jim', 'jules', 'tom', 'tim', 'tam', 'bob', 'barb', 'belle']
+
+  // Mock Math.random with predictable results
+  const RANDOM_RESULTS = [0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1, 0]
+  const mockRandomFn = () => RANDOM_RESULTS.shift() ?? 0
+
+  const output = randomizeStringArray(input, mockRandomFn)
+
+  assertEquals(input.length, output.length)
+  assertEquals(new Set(input), new Set(output))
+  assertNotStrictEquals(input, output)
+  assertEquals(output, ['belle', 'barb', 'bob',   'tam', 'tim',   'tom', 'jules', 'jim', 'jane',  'john'])
+  assertNotEquals(input, output)
 })
--- index.ts
+++ index.ts
@@ -1,3 +1,33 @@
-export function randomizeStringArray (array: string[]) {
-  return [...array]
+export function selectRandomPairs (namesList: string[], randomFn: () => number = Math.random) {
+  if (namesList.length < 3) {
+    return []
+  }
+
+  const randomNamesList = randomizeStringArray(namesList, randomFn)
+
+  return randomNamesList.map((name, index) => {
+    const receivesIndex = index === randomNamesList.length - 1
+      ? 0
+      : index + 1
+
+    return {
+      gives: name,
+      receives: randomNamesList[receivesIndex]
+    }
+  })
+}
+
+export function randomizeStringArray (array: string[], randomFn: () => number = Math.random) {
+  if (array.length < 3) {
+    return [...array]
+  }
+
+  const randomlyWeightedArray = array.map((value) => ({
+    value,
+    compareIndex: randomFn()
+  }))
+
+  return randomlyWeightedArray
+    .sort((arr1, arr2) => arr1.compareIndex - arr2.compareIndex)
+    .map(({ value }) => value)
 }
